b819e4b3b83f25966ecae3faeee7252724b2729f
Yoga Harisistianugroho <yogahariss@gmail.com>
2026-07-30 01:28:36 +0700

    Two Sum

---
+++ src/test/java/yogaharis/solution/array/TwoSumTest.java
@@ -0,0 +1,37 @@
+package yogaharis.solution.array;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TwoSumTest {
+
+    private TwoSum twoSum;
+
+    @BeforeEach
+    void setUp() {
+        twoSum = new TwoSum();
+    }
+
+    @Test
+    void test1() {
+        int[] result = twoSum.twoSum(new int[]{2, 7, 11, 15}, 9);
+
+        assertArrayEquals(new int[]{0,1}, result);
+    }
+
+    @Test
+    void test2() {
+        int[] result = twoSum.twoSum(new int[]{3,2,4}, 6);
+
+        assertArrayEquals(new int[]{1,2}, result);
+    }
+
+    @Test
+    void test3() {
+        int[] result = twoSum.twoSum(new int[]{3,3}, 6);
+
+        assertArrayEquals(new int[]{0,1}, result);
+    }
+}

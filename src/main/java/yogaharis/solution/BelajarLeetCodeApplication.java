9d553e4f3b128c1cef5faf615b2ed80bff15d872
Yoga Harisistianugroho <yogahariss@gmail.com>
2026-07-30 00:52:35 +0700

    initialize project

---
+++ src/main/java/yogaharis/solution/BelajarLeetCodeApplication.java
@@ -0,0 +1,13 @@
+package yogaharis.solution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarLeetCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarLeetCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yogaharis/solution/BelajarLeetCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package yogaharis.solution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarLeetCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
